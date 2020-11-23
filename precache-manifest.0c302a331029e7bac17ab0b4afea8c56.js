ee726aec45eab33626ffd8575fdce7374bc29f6e
SujitSinha <sujitsinha.yo@gmail.com>
2020-11-23 19:10:40 +0530

    Updates

---
+++ precache-manifest.0c302a331029e7bac17ab0b4afea8c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203490040554a982185d35d51bb464e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39c17b69d951ecfa541b",
+    "url": "/static/css/main.c8ad3623.chunk.css"
+  },
+  {
+    "revision": "50c77ff278335f9058b7",
+    "url": "/static/js/2.7a31622e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7a31622e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c17b69d951ecfa541b",
+    "url": "/static/js/main.1ae831a9.chunk.js"
+  },
+  {
+    "revision": "55c65d67cb19f699ec3f",
+    "url": "/static/js/runtime-main.18ca64aa.js"
+  }
+]);
